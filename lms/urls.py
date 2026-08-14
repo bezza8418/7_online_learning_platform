db70e389c35dc814faf36bc4a06b9713f4d30e93
viktor bezuglov <bezza8418@gmail.com>
2026-08-14 22:41:07 +0500

    Реализован CRUD для уроков через Generic-классы

--- lms/urls.py
+++ lms/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet
+from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
+    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-retrieve-update-destroy'),
+]
+
+urlpatterns += router.urls
--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets
 from .models import Course
 from .serializers import CourseSerializer
+from rest_framework import generics
+from .models import Lesson
+from .serializers import LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Course (полный CRUD)"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonListCreateView(generics.ListCreateAPIView):
+    """Получение списка уроков и создание нового урока"""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """Получение, обновление и удаление урока"""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
