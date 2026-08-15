1eaa0005d753953b09d519529785e495bc646928
viktor bezuglov <bezza8418@gmail.com>
2026-08-15 20:43:24 +0500

    Добавлен сериализатор для Payment

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar']
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Payment"""
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
