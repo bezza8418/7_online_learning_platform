37716ccf560fc0cf07a58f694c6f3cf53e667074
viktor bezuglov <bezza8418@gmail.com>
2026-08-15 20:56:50 +0500

    Добавлена история платежей в профиль пользователя

--- users/serializers.py
+++ users/serializers.py
@@ -2,15 +2,16 @@ from rest_framework import serializers
 from .models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar']
-
-
 class PaymentSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Payment"""
-
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для пользователя с историей платежей"""
+    payments = PaymentSerializer(source='payments.all', many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payments']
