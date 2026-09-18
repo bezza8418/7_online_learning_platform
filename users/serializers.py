98641bdff906bb8c48de0d6a8be0b71d88aa347c
viktor bezuglov <bezza8418@gmail.com>
2026-09-18 18:58:42 +0500

    Реализован CRUD для пользователей с регистрацией

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import User, Payment
 
 
@@ -15,3 +16,35 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payments']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Сериализатор для регистрации нового пользователя"""
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password_confirm', 'first_name', 'last_name', 'phone', 'city', 'avatar']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({'password': 'Пароли не совпадают'})
+        return attrs
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Пользователь с таким email уже существует')
+        return value
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -1,25 +1,41 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from .models import User, Payment
-from .serializers import UserSerializer, PaymentSerializer
+from .serializers import (
+    UserSerializer,
+    UserRegistrationSerializer,
+    PaymentSerializer,
+)
 from .filters import PaymentFilter
 
 
+class UserRegistrationView(generics.CreateAPIView):
+    """Регистрация нового пользователя (доступна без авторизации)"""
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+
 class UserListCreateView(generics.ListCreateAPIView):
+    """Список пользователей и создание нового"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """Получение, обновление и удаление пользователя"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class PaymentListCreateView(generics.ListCreateAPIView):
     """Список платежей с фильтрацией и создание нового платежа"""
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = PaymentFilter
     ordering_fields = ['payment_date']
