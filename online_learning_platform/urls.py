42619364024f2484ce9d1b3d1049e459ac393094
viktor bezuglov <bezza8418@gmail.com>
2026-09-18 18:43:20 +0500

    Настроена JWT-авторизация

--- online_learning_platform/urls.py
+++ online_learning_platform/urls.py
@@ -3,12 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
+    # JWT-токены (доступны без авторизации)
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # API
     path('api/', include('lms.urls')),
-    path('api/', include('users.urls')),  # добавляем маршруты для users
+    path('api/', include('users.urls')),
 ]
 
 if settings.DEBUG:
