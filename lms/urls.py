a673593a395d501f901357e9aef8b4de7e0ffa4f
viktor bezuglov <bezza8418@gmail.com>
2026-09-19 10:50:24 +0500

    Реализован эндпоинт для управления подпиской

--- lms/urls.py
+++ lms/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
+from .views import (
+    CourseViewSet,
+    LessonListCreateView,
+    LessonRetrieveUpdateDestroyView,
+    SubscriptionAPIView,
+)
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
@@ -8,6 +13,7 @@ router.register(r'courses', CourseViewSet, basename='course')
 urlpatterns = [
     path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
     path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-retrieve-update-destroy'),
+    path('subscribe/', SubscriptionAPIView.as_view(), name='subscribe'),
 ]
 
 urlpatterns += router.urls
